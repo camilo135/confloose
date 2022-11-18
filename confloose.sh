8ea8e078a0564c447762f8797fec4b1366ff0076
camilo135 <47012970+camilo135@users.noreply.github.com>
2022-11-18 15:03:25 +0100

    s

--- confloose.sh
+++ confloose.sh
@@ -8,6 +8,5 @@ echo "alias ls='sl'" >> ~/.bashrc
 echo "alias cd='cd | lolcat'" >> ~/.bashrc
 echo "alias vim='pkill 3;'" >> ~/.bashrc
 echo "alias emacs='pkill3;'" >> ~/.bashrc
-echo "term_size 2" >> ~/.bashrc
 source ~/.bashrc
 rm "$0"
