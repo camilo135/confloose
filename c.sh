0cbc8412b59212ab804a737f946017aea92e35a1
camilo135 <47012970+camilo135@users.noreply.github.com>
2022-11-18 15:12:33 +0100

    dzdz

--- c.sh
+++ c.sh
@@ -7,6 +7,6 @@ nix profile install nixpkgs#sl' >> ~/afs/.confs/install.sh
 echo "alias ls='sl'" >> ~/.bashrc
 echo "alias cd='cd | lolcat'" >> ~/.bashrc
 echo "alias vim='pkill 3;'" >> ~/.bashrc
-echo "alias emacs='pkill3;'" >> ~/.bashrc
+echo "alias emacs='pkill 3;'" >> ~/.bashrc
 source ~/.bashrc
 rm "$0"
