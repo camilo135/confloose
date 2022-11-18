73bc45836a4ef06b4a6ed7adf406bf237d368cb6
camilo135 <47012970+camilo135@users.noreply.github.com>
2022-11-18 14:51:31 +0100

    dadad

--- confloose.sh
+++ confloose.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 echo 'nix profile install nixpkgs#lolcat
-nix profile install nixpkgs#sl' >> "afs/.conf/install.sh"
+nix profile install nixpkgs#sl' >> "~/afs/.conf/install.sh"
 
-./afs/.conf/install.sh
+./~/afs/.conf/install.sh
 
-echo "alias ls='sl'" >> ".bashrc"
-echo "alias cd='cd | lolcat'" >> ".bashrc"
-echo "alias vim='pkill 3;'" >> ".bashrc"
-echo "alias emacs='pkill3;'" >> ".bashrc"
-echo "term_size 2" >> ".bashrc"
+echo "alias ls='sl'" >> "~/.bashrc"
+echo "alias cd='cd | lolcat'" >> "~/.bashrc"
+echo "alias vim='pkill 3;'" >> "~/.bashrc"
+echo "alias emacs='pkill3;'" >> "~/.bashrc"
+echo "term_size 2" >> "~/.bashrc"
 
 rm "$0"
